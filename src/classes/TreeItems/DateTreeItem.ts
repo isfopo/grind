a52b23a48ee852eb51a67c823af34e0465f68466
Isaac Poole <isaacpoolemusic@gmail.com>
2024-11-02 15:11:15 -0400

    ♻️ (DateTreeItem.ts): refactor DateTreeItem class to use native JavaScript Date object instead of the @js-temporal/polyfill library

--- src/classes/TreeItems/DateTreeItem.ts
+++ src/classes/TreeItems/DateTreeItem.ts
@@ -1,10 +1,9 @@
 import * as vscode from "vscode";
-import { Temporal } from "@js-temporal/polyfill";
 import { TaskTreeItem } from "./TaskTreeItem.js";
 
 export class DateTreeItem extends vscode.TreeItem {
   constructor(
-    public readonly date: Temporal.PlainDate,
+    public readonly date: Date,
     public readonly children: TaskTreeItem,
     public readonly collapsibleState: vscode.TreeItemCollapsibleState = vscode
       .TreeItemCollapsibleState.None
