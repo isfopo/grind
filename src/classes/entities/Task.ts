43548dc15dfbfadcfcca8721eef684ce133ba0a9
Isaac Poole <isaacpoolemusic@gmail.com>
2024-11-09 11:21:44 -0500

    📝 docs(Task): add detailed documentation comments
    - Added JSDoc comments for methods to describe their purpose and usage
    - Improved code readability and maintainability through comprehensive comments

--- src/classes/entities/Task.ts
+++ src/classes/entities/Task.ts
@@ -21,6 +21,12 @@ export class Task {
     this.subtasks = tasks ?? [];
   }
 
+  /**
+   * Converts the Task instance into a JSON string representation.
+   * This includes the task's ID, day, label, subtasks, and completion status.
+   *
+   * @returns {string} - A JSON string representing the Task.
+   */
   stringify(): string {
     return JSON.stringify({
       id: this.id,
@@ -31,16 +37,40 @@ export class Task {
     });
   }
 
+  /**
+   * Toggles the completion status of the task.
+   * This method updates the 'completed' property to its opposite value (true to false or false to true).
+   * It returns the current instance of the Task class after the update.
+   *
+   * @returns {Task} - The current Task instance with the updated completion status.
+   */
   toggleCompleted(): Task {
     this.completed = !this.completed;
     return this;
   }
 
+  /**
+   * Parses a JSON string and converts it into a Task instance.
+   * This method takes a JSON string representation of a Task and reconstructs
+   * the Task object, ensuring that it has the correct type and properties.
+   *
+   * @param {string} json - A JSON string representing a Task.
+   * @returns {Task} - An instance of the Task class constructed from the JSON string.
+   * @throws {SyntaxError} - Thrown if the input string is not valid JSON.
+   */
   static parse(json: string): Task {
     return JSON.parse(json) as Task;
   }
 
-  static generateTaskId(length: number = 21): string {
+  /**
+   * Generates a unique task ID as a string.
+   * This method creates a task ID by combining the current timestamp
+   * (in base 36) and a random number (also in base 36) that falls
+   * within a specific range to ensure uniqueness.
+   *
+   * @returns {string} - A unique task ID as a string.
+   */
+  static generateTaskId(): string {
     return (
       Date.now().toString(36) +
       Math.floor(
@@ -49,11 +79,30 @@ export class Task {
     );
   }
 
+  /**
+   * Validates a given task ID by checking if it matches the expected format.
+   * The format requires the task ID to consist of a total of 25 characters,
+   * where the first 13 characters are alphanumeric (0-9, a-z) followed by
+   * another 12 alphanumeric characters. This method returns true if the key
+   * conforms to this pattern; otherwise, it returns false.
+   *
+   * @param {string} key - The task ID to validate.
+   * @returns {boolean} - True if the task ID is valid, false otherwise.
+   */
   static validate(key: string): boolean {
     return /^[0-9a-z]{13}[0-9a-z]{12}$/.exec(key) !== null;
   }
 
-  addSubtask() {
+  /**
+   * Adds a new subtask to the current task.
+   * This method generates a unique task ID for the subtask using the
+   * `generateTaskId` method and then appends this ID to the
+   * `subtasks` array of the current task.
+   * It returns the ID of the newly added subtask.
+   *
+   * @returns {TaskId} - The ID of the newly created subtask.
+   */
+  addSubtask(): TaskId {
     const id = Task.generateTaskId();
     this.subtasks.push(id);
     return id;
