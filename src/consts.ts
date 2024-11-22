85a0dffdfb13bf1c458910fc7d5cd43a99d2e9b7
Isaac Poole <isaacpoolemusic@gmail.com>
2024-11-21 19:01:44 -0500

    ✨ feat(consts): add export format option
    - introduce EXPORT_FORMAT constant to choose between "richtext" and "markdown" formats
    - enables more flexibility in content export options

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,4 @@
 export const ENABLE_COMPLETIONS = false;
+export const EXPORT_FORMAT: "richtext" | "markdown" = "richtext";
 
 export const STORAGE_SCOPE: "global" | "workspace" = "workspace"; // Allow this to be set to workplace storage via settings
